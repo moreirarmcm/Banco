52ba654fe22bd5a6c28241bb99713fefb352f24f
Renan Moreira <105755795+moreirarmcm@users.noreply.github.com>
2022-07-28 00:11:52 -0300

    Update banco.sql

--- database/banco.sql
+++ database/banco.sql
@@ -1,10 +1,22 @@
 CREATE DATABASE banco;
 
-CREATE TABLE cliente (
-	codigo INT,
-	nome VARCHAR(20),
-	email,
-	cpf,
-	data_nascimento,
-	data_cadastro
-	);
+USE banco;
+
+CREATE TABLE cliente(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR (30),
+	email VARCHAR(30),
+	cpf CHAR(11),
+	data_nascimento DATE,
+	data_cadastro DATE
+);
+	
+CREATE TABLE conta(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	id_cliente INT,
+	saldo FLOAT(11,2),
+	limite FLOAT(11,2)
+);
+
+	
+	
